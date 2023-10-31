f8447c0a39e987b3ed56f4b5b367d76a04fbd5d2
Jian-Long Huang <contact@jlhg.org>
2023-11-01 06:01:34 +0800

    巴哈姆特-自動展開留言: 支援自動展開GNN新聞留言

---
+++ script/巴哈姆特-自動展開留言/script.user.js
@@ -0,0 +1,51 @@
+// ==UserScript==
+// @name         巴哈姆特-自動展開留言
+// @namespace    https://github.com/jlhg/userscript
+// @license      MIT
+// @version      0.2.0
+// @description  自動展開巴哈姆特哈拉區、GNN新聞文章底下所有留言
+// @author       jlhg
+// @homepage     https://github.com/jlhg/userscript
+// @supportURL   https://github.com/jlhg/userscript/issues
+// @match        https://forum.gamer.com.tw/C.php*
+// @match        https://forum.gamer.com.tw/Co.php*
+// @match        https://gnn.gamer.com.tw/detail.php*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+  'use strict';
+
+  function wait(selector) {
+    return new Promise(resolve => {
+      if (document.querySelector(selector)) {
+        return resolve(document.querySelector(selector));
+      }
+
+      const observer = new MutationObserver(mutations => {
+        if (document.querySelector(selector)) {
+          observer.disconnect();
+          resolve(document.querySelector(selector));
+        }
+      });
+
+      observer.observe(document.body, {
+        childList: true,
+        subtree: true
+      });
+    });
+  }
+
+  switch (window.location.host) {
+    case "forum.gamer.com.tw":
+      document.querySelectorAll("[id^=showoldCommend_]")
+              .forEach((el) => el.click());
+      break;
+    case "gnn.gamer.com.tw":
+      wait('#comment a[href^="javascript:get_all_comment"]').then((e) => {
+        eval($(e).attr('href').split(':')[1]);
+      });
+      break;
+  }
+
+})();
