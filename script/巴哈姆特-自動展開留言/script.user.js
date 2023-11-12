5ea208083363e49cea65cf2c80fb57b23252d80c
Jian-Long Huang <contact@jlhg.org>
2023-11-13 01:54:41 +0800

    Fix indentation

--- script/巴哈姆特-自動展開留言/script.user.js
+++ script/巴哈姆特-自動展開留言/script.user.js
@@ -2,7 +2,7 @@
 // @name         巴哈姆特-自動展開留言
 // @namespace    https://github.com/jlhg/userscript
 // @license      MIT
-// @version      0.2.1
+// @version      0.2.2
 // @description  自動展開巴哈姆特哈拉區、GNN新聞文章底下所有留言
 // @author       jlhg
 // @homepage     https://github.com/jlhg/userscript
@@ -37,15 +37,15 @@
   }
 
   switch (window.location.host) {
-    case "forum.gamer.com.tw":
-      document.querySelectorAll("[id^=showoldCommend_]")
-              .forEach((el) => el.click());
-      break;
-    case "gnn.gamer.com.tw":
-      waitElement('#comment a[href^="javascript:get_all_comment"]').then((e) => {
-        eval($(e).attr('href').split(':')[1]);
-      });
-      break;
+  case "forum.gamer.com.tw":
+    document.querySelectorAll("[id^=showoldCommend_]")
+      .forEach((el) => el.click());
+    break;
+  case "gnn.gamer.com.tw":
+    waitElement('#comment a[href^="javascript:get_all_comment"]').then((e) => {
+      eval($(e).attr('href').split(':')[1]);
+    });
+    break;
   }
 
 })();
