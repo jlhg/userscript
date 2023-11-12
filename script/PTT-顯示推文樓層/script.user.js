74c1fe21b05c5b874d8e0a5ef2f2bb9e311b8ba8
Jian-Long Huang <contact@jlhg.org>
2023-11-12 14:03:04 +0800

    Add new script: PTT 顯示推文樓層

---
+++ script/PTT-顯示推文樓層/script.user.js
@@ -0,0 +1,36 @@
+// ==UserScript==
+// @name         PTT 顯示推文樓層
+// @namespace    https://github.com/jlhg/userscript
+// @license      MIT
+// @version      0.1.0
+// @description  PTT 顯示推文樓層
+// @author       jlhg
+// @homepage     https://github.com/jlhg/userscript
+// @supportURL   https://github.com/jlhg/userscript/issues
+// @match        https://www.ptt.cc/bbs/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+  'use strict';
+
+  function createPushLevel(level) {
+    const el = document.createElement('div');
+    el.className = 'push-level';
+    el.style.cssText = 'display: inline-block;' +
+      'min-width: 50px;' +
+	    'margin-left: -62px;' +
+	    'padding-right: 12px;' +
+	    'text-align: right;' +
+	    'font-family: monospace;' +
+	    'color: #808080;';
+    el.textContent = level;
+    return el
+  }
+
+  const pushElements = document.querySelectorAll('.push');
+
+  for (let i = 0; i < pushElements.length; i++) {
+    pushElements[i].prepand(createPushLevel(i + 1));
+  }
+})();
